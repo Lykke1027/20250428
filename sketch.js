13e1ed8335758b3eb76ea0545d1464bc8d26ffea
Lykke1027 <call34632435@gmail.com>
2025-04-28 10:51:47 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,27 +10,31 @@ function setup() {
 
   // 建立與攝影機影像一樣大小的 overlayGraphics
   overlayGraphics = createGraphics(capture.width, capture.height);
-  drawOverlayGraphics();
 }
 
 function draw() {
   background(245, 245, 220); // 確保背景為米色
-  translate(width, 0); // 將畫布的原點移到右上角
-  scale(-1, 1); // 水平翻轉畫布
 
-  // 顯示攝影機影像
-  image(
-    capture,
-    (width - capture.width) / 2, // 計算影像水平置中位置
-    (height - capture.height) / 2 // 計算影像垂直置中位置
-  );
+  // 檢查攝影機是否準備好
+  if (capture.loadedmetadata) {
+    translate(width, 0); // 將畫布的原點移到右上角
+    scale(-1, 1); // 水平翻轉畫布
 
-  // 顯示 overlayGraphics 在攝影機影像上方
-  image(
-    overlayGraphics,
-    (width - capture.width) / 2, // 與攝影機影像水平對齊
-    (height - capture.height) / 2 // 與攝影機影像垂直對齊
-  );
+    // 顯示攝影機影像
+    image(
+      capture,
+      (width - capture.width) / 2, // 計算影像水平置中位置
+      (height - capture.height) / 2 // 計算影像垂直置中位置
+    );
+
+    // 更新 overlayGraphics 並顯示在攝影機影像上方
+    drawOverlayGraphics();
+    image(
+      overlayGraphics,
+      (width - capture.width) / 2, // 與攝影機影像水平對齊
+      (height - capture.height) / 2 // 與攝影機影像垂直對齊
+    );
+  }
 }
 
 function windowResized() {
@@ -39,7 +43,6 @@ function windowResized() {
 
   // 更新 overlayGraphics 的大小
   overlayGraphics = createGraphics(capture.width, capture.height);
-  drawOverlayGraphics();
 }
 
 function drawOverlayGraphics() {
